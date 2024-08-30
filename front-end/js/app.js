42d4be9c5592394ddc777e1f52306b2ac0a8f1da
Debabrata pattnayak <pattnaikd833@gmail.com>
2024-08-31 00:04:16 +0530

    login page front-end complet

---
+++ front-end/js/app.js
@@ -0,0 +1,10 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
+
+function scrollToMap() {
+    document.getElementById('map-section').scrollIntoView({ behavior: 'smooth' });
+}
